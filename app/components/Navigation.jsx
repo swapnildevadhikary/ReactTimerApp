1621f2fa7dbe748600ef5e3902245692c702c870
Adhikary <SAdhikary@ad.harman.com>
2016-09-07 18:08:20 +0530

    Initial commit

---
+++ app/components/Navigation.jsx
@@ -0,0 +1,39 @@
+var React = require('react');
+var {Link,IndexLink}=require('react-router');
+
+
+var Navigation=()=>{
+  return(
+    <div className="top-bar">
+      <div className="top-bar-left">
+        <ul className="menu">
+          <li className="menu-text">
+            React Timer App
+
+          </li>
+          <li className="menu-text">
+            <IndexLink to="/" activeClassName="active-link">Timer</IndexLink>
+
+          </li>
+          <li className="menu-text">
+            <Link to="/" activeClassName="active-link">Countdown</Link>
+
+          </li>
+        </ul>
+      </div>
+      <div className="top-bar-right">
+           <ul className="menu">
+             <li className="menu-text">
+               Created by<a href="http://www.mead.io" target="_blank">Andrew Mead</a>
+
+             </li>
+
+
+           </ul>
+      </div>
+
+    </div>
+  );
+};
+
+module.exports=Navigation;
